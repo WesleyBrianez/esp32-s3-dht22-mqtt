f4c270f85b788aaf5b71db5d682f1b99c43f7def
Wesley Brianez <wesleybrianez@gmail.com>
2024-06-01 17:30:02 -0300

    added button state machine

---
+++ api_structs.h
@@ -0,0 +1,37 @@
+#ifndef api_structs_h
+#define api_structs_h
+
+//enums----------------------------
+typedef enum
+{
+  GREEN = 0,
+  YELLOW,
+  RED,
+  BLUE,
+  LED_COLLOR_MAX_INDEX
+}eLedCollor;
+
+typedef enum
+{
+  STATE_OFF = 0,
+  STATE_ON,
+  STATE_BLINK_1000_MS,
+  STATE_BLINK_300_MS,
+  LED_STATE_MAX_INDEX
+}eLedState;
+
+typedef enum
+{
+  UNPRESSED = 0,
+  PRESSED,
+  BT_STATE_MAX_INDEX
+}eButtonState;
+
+//structs---------------------------
+typedef struct
+{
+  eButtonState state;
+  volatile TickType_t tickCount;
+}sButton;
+
+#endif
--- sketch.ino
+++ sketch.ino
@@ -1,12 +1,18 @@
 #include "DHT.h"
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include "api_structs.h"
 
-#define DHT_PIN   16
-#define DHT_TYPE  DHT22
-#define BT_PIN    GPIO_NUM_14
+#define DHT_PIN       16
+#define DHT_TYPE      DHT22
+#define BT_PIN        GPIO_NUM_14
+#define IS_BT_PRESSED !gpio_get_level(BT_PIN)
 
 DHT dht (DHT_PIN, DHT_TYPE);
+
+sButton button;
+uint8_t led_collor; //eLedCollor
+uint8_t led_state; //eLedState
 
 TaskHandle_t rgb_h;
 TaskHandle_t dht_h;
@@ -15,41 +21,28 @@ TaskHandle_t state_h;
 
 void setup()
 {
+  setCpuFrequencyMhz(160); //Set CPU frequency to 160 MHz
+
   pinMode(BT_PIN, INPUT_PULLUP);
-  Serial.begin(115200);
+  button.state = UNPRESSED;
 
   xTaskCreatePinnedToCore(pvTask_Rgb,          "rgb_h",    2048, NULL, 1, &rgb_h,    APP_CPU_NUM);
   xTaskCreatePinnedToCore(pvTask_Dht22,        "dht_h",    2048, NULL, 1, &dht_h,    APP_CPU_NUM);
   xTaskCreatePinnedToCore(pvTask_Button,       "bt_h",     2048, NULL, 1, &bt_h,     PRO_CPU_NUM);
   xTaskCreatePinnedToCore(pvTask_StateReport,  "state_h",  2048, NULL, 1, &state_h,  PRO_CPU_NUM);
 
+  Serial.begin(115200);
   dht.begin();
 }
 
 void loop()
 {
-    Serial.print(__func__);
-    Serial.print(" : ");
-    Serial.print(xTaskGetTickCount());
-    Serial.print(" : ");
-    Serial.print("Arduino loop is running on core:");
-    Serial.println(xPortGetCoreID());
-    Serial.println();
 }
 
 void pvTask_Rgb(void *arg)
 {
   while(1)
   {
-    Serial.print(__func__);
-    Serial.print(" : ");
-    Serial.print(xTaskGetTickCount());
-    Serial.print(" : ");
-    Serial.print("This loop runs on APP_CPU which id is:");
-    Serial.println(xPortGetCoreID());
-    Serial.println();
-
-    vTaskDelay(100);
   }
 }
 
@@ -57,28 +50,60 @@ void pvTask_Dht22(void *arg)
 {
   while(1)
   {
-    TempAndHum();
-
-    Serial.print(__func__);
-    Serial.print(" : ");
-    Serial.print(xTaskGetTickCount());
-    Serial.print(" : ");
-    Serial.print("This loop runs on APP_CPU which id is:");
-    Serial.println(xPortGetCoreID());
-    Serial.println();
-
-    vTaskDelay(100);
   }
 }
 
 void pvTask_Button(void *arg)
 {
+  bool bt = 0;
+
   while(1)
   {
-    Serial.print("button:");
-    Serial.println(digitalRead(BT_PIN));
+    if(millis()-button.tickCount >= 100) //bounce
+    {
+      bt = IS_BT_PRESSED;
 
-    vTaskDelay(100);
+      switch(button.state)
+      {
+        case UNPRESSED:
+        if(bt)
+        {
+          button.state = PRESSED;
+          button.tickCount = millis(); //xTaskGetTickCount()
+          Serial.println("bt PRESSED");
+        }
+        break;
+
+        case PRESSED:
+        if(!bt)
+        {
+          Serial.println(millis()-button.tickCount);
+          
+          //pressed above 3s, change led_collor
+          if(millis()-button.tickCount >= 3000)
+          {
+            Serial.print("bt 3s - led_collor:");
+            if(++led_collor >= LED_COLLOR_MAX_INDEX)
+              led_collor = GREEN;
+
+            Serial.println(led_collor);
+          }
+          //pressed below than 3s, change led_state
+          else
+          {
+            Serial.print("bt 1s - led_state:");
+            if(++led_state >= LED_STATE_MAX_INDEX)
+              led_state = STATE_OFF;
+
+            Serial.println(led_state);
+          }
+
+          button.state = UNPRESSED;
+          button.tickCount = millis(); //xTaskGetTickCount()
+        }
+        break;
+      }
+    }
   }
 }
 
@@ -86,15 +111,6 @@ void pvTask_StateReport(void *arg)
 {
   while(1)
   {
-    Serial.print(__func__);
-    Serial.print(" : ");
-    Serial.print(xTaskGetTickCount());
-    Serial.print(" : ");
-    Serial.print("This loop runs on PRO_CPU which id is:");
-    Serial.println(xPortGetCoreID());
-    Serial.println();
-
-    vTaskDelay(100);
   }
 }
 
@@ -117,4 +133,16 @@ void TempAndHum()
     Serial.println(" *C");
   }
 }
+
+/*
+    Serial.print(__func__);
+    Serial.print(" : ");
+    Serial.print(xTaskGetTickCount());
+    Serial.print(" : ");
+    Serial.print("This loop runs on PRO_CPU which id is:");
+    Serial.println(xPortGetCoreID());
+    Serial.println();
+
+    vTaskDelay(100);
+*/
 
